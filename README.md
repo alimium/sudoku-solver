c6c25b81be505fb3df57b691aea500b36f4bb34d
Ali Mehrabi <alimehrabi002@gmail.com>
2023-07-19 16:47:51 +0330

    Add README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# Sudoku Game / Solver
+
+> *This is a project for Aritficial Intelligence course (1319104) at Amirkabir University of Thechnology. This course aligns with CS188 Artificial Intelligence at UC Berkeley.*
+
--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from colorama import Fore
 
 def parse_input():
     os.system("cls" if os.name == "nt" else "clear")
-    size: int = int(input(f"{Fore.CYAN}Enter size of sudoku (Ex: 9):{Fore.RESET} "))
+    size: int = int(
+        input(f"{Fore.CYAN}Enter size of sudoku (Ex: 9):{Fore.RESET} "))
     game: np.array = np.zeros((size, size), dtype=int)
     for i in range(size):
         row: str = input(f"Enter row {i + 1}: ").split(" ")
@@ -25,13 +26,16 @@ def main():
     sdk = SudokuSamples.arto_inkala_9
 
     # uncomment next line for custom input
-    # sdk = parse_input()
+    sdk = parse_input()
 
     game = SudokuGame(sdk)
+    # game.draw()
     solver = BackTrackSolver(game)
     result = solver.solve(
         step_by_step=False, selection=SelectionMethod.MOST_CONSTRAINED
     )
+
+    print(result['solved'])
 
     # solver.display_step_by_step(manual=True) # uncomment this line to see the steps of the solver
     # print("\n" + str(result["solved"])) # uncomment this line to see the solved sudoku
